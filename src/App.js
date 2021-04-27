1ed891f831fe3ed6130933b0630c6482af7f8fe5
kstick9210 <kstick9210@outlook.com>
2021-04-27 10:34:52 -0500

    set up network request for ride details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+import './App.css';
+import YourTrip from './components/YourTrip/YourTrip';
+
+export default function App() {
+  const [ rideDetails, setRideDetails ] = useState("");
+
+  const getRideDetails = () => {
+    axios.get("http://localhost:3000/mission.json")
+    .then(response => setRideDetails(response.data));
+  }
+
+  useEffect(() => {
+    getRideDetails();
+  }, [])
+
   return (
     <div className="App">
+      <YourTrip />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/YourTrip/YourTrip.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './YourTrip.css';
+
+export default function YourTrip() {
+    return (
+        <div>
+            Your Trip
+        </div>
+    )
+}
